8ff8210531657b4b07358ba0b5734de5de185d34
kamaha12000 <kamaha12000@yahoo.fr>
2018-12-31 06:17:33 -0800

    Login endpoint and functionality to generate Jwt token to user

---
+++ GiveNTake/Controllers/AccountController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using GiveNTake.Model;
+using GiveNTake.Model.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GiveNTake.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Account")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationUserDTO registration)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.FindByEmailAsync(registration.Email);
+            if (user != null)
+            {
+                SerializableError serializableError = new SerializableError { { nameof(registration.Email), "Email already exist in the system" } };
+                return this.StatusCode((int)HttpStatusCode.Conflict, serializableError);
+            }
+
+            User newUser = new User
+            {
+                Email = registration.Email,
+                UserName = registration.Email,
+                Id = registration.Email
+            };
+
+            var result = await _userManager.CreateAsync(newUser, registration.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+
+            //user = await _userManager.FindByEmailAsync(registration.Email);
+            return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<SuccessfulLoginResult>> Login([FromBody] LoginUserDTO login)
+        {
+            SignInResult result = await _signInManager.PasswordSignInAsync(login.Email, login.Password, false, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized();
+            }
+
+            User user = await _userManager.FindByEmailAsync(login.Email);
+            JwtSecurityToken token = GenerateTokenAsync(user);  //defined
+            string serializedToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new SuccessfulLoginResult() { Token = serializedToken });
+        }
+
+        private JwtSecurityToken GenerateTokenAsync(User user)
+        {
+            var claims = new List<Claim>();      //Loading user Claims
+
+            var expirationDays = _configuration.GetValue<int>("JWTConfiguration:TokenExpirationDays");
+            var signingKey = Encoding.UTF8.GetBytes(_configuration.GetValue<string>("JWTConfiguration:SigningKey"));
+            var token = new JwtSecurityToken(
+                issuer: _configuration.GetValue<string>("JWTConfiguration:Issuer"),
+                audience: _configuration.GetValue<string>("JWTConfiguration:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.Add(TimeSpan.FromDays(expirationDays)),
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(signingKey), SecurityAlgorithms.HmacSha256)
+                );
+
+            return token;
+        }
+    }
+}
