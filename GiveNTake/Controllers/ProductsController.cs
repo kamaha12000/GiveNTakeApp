b341ff898add8c10c286f35aa6f25e1452898b91
kamaha12000 <kamaha12000@yahoo.fr>
2018-12-30 17:02:25 -0800

    Added more endpoints to the rest server.

--- GiveNTake/Controllers/ProductsController.cs
+++ GiveNTake/Controllers/ProductsController.cs
@@ -40,11 +40,6 @@ namespace GiveNTake.Controllers
                 cfg.CreateMap<Category, SubCategoryDTO>();
             });
 
-
-            
-
-
-
             _productsMapper = config.CreateMapper();
         }
         public ProductsController(GiveNTakeContext context)
@@ -127,6 +122,47 @@ namespace GiveNTake.Controllers
                 );
         }
 
+        [HttpDelete("{productId}")]
+        public async Task<ActionResult> DeleteProduct(int productId)
+        {
+            var product = await _context.Products.SingleOrDefaultAsync(p => p.ProductId == productId);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{productId}")]
+        public async Task<ActionResult> UpdateProduct(int productId, [FromBody] NewProductDTO newProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            Category category = _context.Categories
+                .SingleOrDefault(c => c.Name == newProduct.SubCategory || c.ParentCategory.Name == newProduct.Category);
+            if (category == null)
+            {
+                return new BadRequestObjectResult("The provided category and sub category doesnt exist");
+            }
+            City city = _context.Cities.SingleOrDefault(c => c.Name == newProduct.City);
+            if (city == null)
+            {
+                return new BadRequestObjectResult("The provided city doesnt exist");
+            }
+            var product = new Product()
+            {
+                ProductId = productId,
+                Category = category,
+                Title = newProduct.Title,
+                Description = newProduct.Description,
+                City = city,
+                PublishDate = DateTime.UtcNow
+            };
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
         [HttpGet("categories")]
         public async Task<ActionResult<CategoryDTO[]>> GetCategoriesAsync()
         {
@@ -137,21 +173,112 @@ namespace GiveNTake.Controllers
             return _productsMapper.Map<CategoryDTO[]>(categories);
         }
 
-        [HttpGet("searchcategory/{category}/{subcategory=all}/")]
-        public string[] SearchByProducts(string category, string subcategory, string location = "all", bool imageOnly = false)
+        [HttpGet("cities")]
+        public async Task<ActionResult<string[]>> GetCities()
         {
-            return new[] { $"Category: {category}, Subcategory: {subcategory}, Location: {location}, Only with Images: {imageOnly}" };
+            var cities = await _context.Cities
+                .Select(c => c.Name)
+                .ToArrayAsync();
+            return cities;
+        }
+
+        [HttpPost("categories")]
+        public async Task<ActionResult> AddCategory([FromBody] NewCategoryDTO newCategoryDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var category = await _context.Categories
+                .Include(c => c.SubCategories)
+                .SingleOrDefaultAsync(c => c.Name == newCategoryDTO.CategoryName && c.ParentCategory == null);
+
+            if (!string.IsNullOrWhiteSpace(newCategoryDTO.SubCategoryName))         //Adding a subCategory
+            {
+                if (category == null)
+                {
+                    return NotFound(new SerializableError() { { nameof(newCategoryDTO.CategoryName), "Category not Found"} });
+                }
+                if(category.SubCategories.Any(c=>c.Name == newCategoryDTO.CategoryName)) //Subcategorie already exists
+                {
+                    return Ok();
+                }
+                category.SubCategories.Add(new Category { Name = newCategoryDTO.SubCategoryName, ParentCategory = category });
+                
+            }
+            else if (category == null)    //Adding a new category
+            {
+                _context.Categories.Add(new Category { Name = newCategoryDTO.CategoryName });
+            }
+
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet("search/{keyword}")]
+        public async Task<ActionResult<ProductDTO[]>> SearchProducts(string keyword)
+        {
+            var products = await _context.Products
+                            .Include(p => p.Owner)
+                            .Include(p => p.City)
+                            .Include(p => p.Category)
+                            .ThenInclude(c => c.SubCategories)
+                            .Where(p => p.Title.Contains(keyword))
+                            .ToListAsync();
+
+            return _productsMapper.Map<ProductDTO[]>(products);
+        }
+
+        [HttpGet("searchcategory/{category}/{subcategory=all}/")]
+        public async Task<ActionResult<ProductDTO[]>> SearchByProducts(string category, string subcategory, string location = "all", bool imageOnly = false)
+        {
+            if (string.IsNullOrEmpty(category))
+            {
+                return BadRequest();
+            }
+
+            IQueryable<Product> productsQuery = _context.Products
+                .Include(p => p.Owner)
+                .Include(p => p.City)
+                .Include(p => p.Media)
+                .Include(p => p.Category)
+                .ThenInclude(c => c.ParentCategory);
+
+            if (location != "all")
+            {
+                productsQuery = productsQuery.Where(p => p.City.Name == location);
+            }
+
+            if (subcategory != "all")
+            {
+                productsQuery = productsQuery.Where(p => p.Category.Name == subcategory)
+                    .Where(p => p.Category.ParentCategory.Name == category);
+            }
+            else
+            {
+                productsQuery = productsQuery.Where(p => p.Category.Name == category || p.Category.ParentCategory.Name == category);
+            }
+            var products = await productsQuery.ToListAsync();
+
+            return Ok(_productsMapper.Map<ProductDTO[]>(products));
         }
 
         [HttpGet("search/{date:datetime}/{keyword}/")]
-        public string[] Search(string date, string keyword)
+        public async Task<ActionResult<ProductDTO[]>> Search(DateTime date, string keyword)
         {
-            return new[]
-            {
-                $"Date: {date}, keyword: {keyword}"
-            };
-        }
+            var products = await _context.Products
+                .Include(p => p.Owner)
+                .Include(p => p.City)
+                .Include(p => p.Category)
+                .ThenInclude(c => c.ParentCategory)
+                .Where(p => p.Title.Contains(keyword))
+                .Where(p => p.PublishDate.Date == date.Date)
+                .ToListAsync();
 
+            return _productsMapper.Map<ProductDTO[]>(products);
+        }
 
     }
 }
---
+++ GiveNTake/Model/DTO/NewCategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiveNTake.Model.DTO
+{
+    public class NewCategoryDTO
+    {
+        [Required]
+        public string CategoryName { get; set; }
+        public string SubCategoryName { get; set; }
+        
+    }
+}
